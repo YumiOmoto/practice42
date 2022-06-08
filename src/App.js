f2337e2a7cba3dd82f75d7da4695b6151b180714
YumiOmoto <yumi.omoto@play.jp>
2022-06-08 19:11:24 +0900

    パスワードの実装

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+function Password({value, onChange}){
+  const [type, setType] = React.useState('password');
+  const handleClick = () => setType(
+    type === 'password' ? 'text' : 'password' 
+  );
+
+  return (
+    <React.Fragment>
+    <input type={type} value={value} onChange={onChange} />
+    <button type="button" onClick={handleClick}>
+      {type === 'password' ? '見る' : '隠す'}
+    </button>
+    </React.Fragment>
+  );
+}
 
 function App() {
+  const [val, setVal] = React.useState('');
+
+  const handleChange = e => setVal(e.target.value);
+  //　以降表示方式
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <p>パスワード</p>
+    <Password value={val} onChange={handleChange} />
+    <p>{val.length}文字</p>
+    </React.Fragment>
   );
 }
 
 export default App;
+// const root = document.getElementById('root');
+// ReactDOM.render(<App />, root);
